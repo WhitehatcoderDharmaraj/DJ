245440a2bf8c49f93e3b8d82e153ad312bf24f61
Dharmaraj <90249634+WhitehatcoderDharmaraj@users.noreply.github.com>
2022-05-16 17:32:32 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+song1="";
+song2="";
+leftwrX=0;
+rightwrX=0;
+leftwrY=0;
+rightwrY=0;
+
+function preload(){
+    song1=loadSound("Legends Never Die.mp3");
+    song2=loadSound("Beliver.mp3");
+}
+
 function setup(){
 canvas=createCanvas(300,300);
 canvas.center();
@@ -5,6 +17,27 @@ canvas.center();
 video=createCapture(VIDEO);
 video.size(300,300);
 video.hide();
+
+poseNet=ml5.poseNet(video, modelloaded);
+poseNet.on('pose', gotPoses);
+}
+
+function modelloaded(){
+console.log("Pose Net Initialized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        right_wr_x=results[0].pose.rightWrist.x;
+        right_wr_y=results[0].pose.rightWrist.y;
+        console.log("X position of right wrist is "+right_wr_x+" y position of right wrist is "+right_wr_y);
+
+        left_wr_x=results[0].pose.leftWrist.x;
+        left_wr_y=results[0].pose.leftWrist.y;
+        console.log("X   Position of left wrist is "+left_wr_x+" And Y Position is "+left_wr_y);
+    }
+
 }
 
 function draw(){
